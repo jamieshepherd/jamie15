f1796ba7827cde1e2f6ff7732d45da5f714aa83f
Jamie Shepherd <email@jamie.sh>
2015-03-12 23:37:32 +0000

    Completed design for projects page

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -6,14 +6,14 @@
         </header>
         <section>
             <div class="project">
-                <img class="preview" src="/images/tygr.jpg"/>
+                <img class="preview" src="/images/tygr.jpg" />
                 <div class="description">
                     <h2>Tygr</h2>
                     <h3>Issue and project management</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <p><strong>Technologies:</strong> <a href="">Laravel 5</a>, <a href="">PHP</a>, <a href="">MySQL</a>, <a href="">HTML5</a>, <a href="">CSS3</a></p>
 
-                    <a href="" class="box">View project</a>
+                    <a href="" class="box"><i class="fa fa-arrow-circle-right"></i> View project</a>
                     <a href="" class="box"><i class="fa fa-github"></i> Github</a>
                 </div>
             </div>
@@ -21,14 +21,14 @@
 
         <section>
             <div class="project">
-                <img class="preview" src="http://placehold.it/1050x575"/>
+                <img class="preview" src="/images/panther.jpg" />
                 <div class="description">
-                    <h2>Tygr</h2>
-                    <h3>Issue and project management</h3>
+                    <h2>Property Panther</h2>
+                    <h3>Desktop and mobile property management</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p><strong>Technologies:</strong> <a href="">Laravel 5</a>, <a href="">PHP</a>, <a href="">MySQL</a>, <a href="">HTML5</a>, <a href="">CSS3</a></p>
+                    <p><strong>Technologies:</strong> <a href="">Laravel 4.2</a>, <a href="">PHP</a>, <a href="">MySQL</a>, <a href="">HTML5</a>, <a href="">CSS3</a></p>
 
-                    <a href="" class="box">View project</a>
+                    <a href="" class="box"><i class="fa fa-arrow-circle-right"></i> View project</a>
                     <a href="" class="box"><i class="fa fa-github"></i> Github</a>
                 </div>
             </div>
@@ -36,14 +36,29 @@
 
         <section>
             <div class="project">
-                <img class="preview" src="http://placehold.it/1050x575"/>
+                <img class="preview" src="/images/adaptionary.jpg" />
                 <div class="description">
-                    <h2>Tygr</h2>
-                    <h3>Issue and project management</h3>
+                    <h2>Adaptionary</h2>
+                    <h3>Adapt elearning plugin for Sublime Text 3</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p><strong>Technologies:</strong> <a href="">Laravel 5</a>, <a href="">PHP</a>, <a href="">MySQL</a>, <a href="">HTML5</a>, <a href="">CSS3</a></p>
+                    <p><strong>Technologies:</strong> <a href="">XML</a>, <a href="">Sublime Text 3</a></p>
 
-                    <a href="" class="box">View project</a>
+                    <a href="" class="box"><i class="fa fa-arrow-circle-right"></i> View project</a>
+                    <a href="" class="box"><i class="fa fa-github"></i> Github</a>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="project">
+                <img class="preview" src="/images/allaspects.jpg" />
+                <div class="description">
+                    <h2>All Aspects Gardening</h2>
+                    <h3>Local website for landscape gardening services</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p><strong>Technologies:</strong> <a href="">HTML</a>, <a href="">CSS</a></p>
+
+                    <a href="" class="box"><i class="fa fa-arrow-circle-right"></i> View project</a>
                     <a href="" class="box"><i class="fa fa-github"></i> Github</a>
                 </div>
             </div>
