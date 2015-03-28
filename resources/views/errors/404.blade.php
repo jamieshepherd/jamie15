4620928b965c9217838d296eac647e4a6ba8983f
Jamie Shepherd <email@jamie.sh>
2015-03-28 23:05:59 +0000

    Words

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,12 +2,12 @@
 @section('body')
     <header style="background-image: url('/images/404.jpg')">
         <h1>404</h1>
-        <p>Page not found</p>
+        <p>Page could not be found</p>
     </header>
     <section>
         <div class="article">
             <h2>Well this is a little embarassing</h2>
-            <p>I couldn't find the page you were looking for. Sorry about that. If you want to help, <a href="mailto:hello&amp;jamie.sh">shoot me an email</a>, otherwise try the navigation to the left of the page!</p>
+            <p>I couldn't find the page you were looking for. Sorry about that. If you want to help, <a href="mailto:hello&amp;jamie.sh">shoot me an email</a>, otherwise try the navigation menu (open it by clicking the menu at the top left of the page!)</p>
         </div>
     </section>
 @stop
