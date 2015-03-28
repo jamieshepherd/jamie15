9c896ec0e730b7765731d5359db81820ef39fa2a
Jamie Shepherd <email@jamie.sh>
2015-03-28 23:03:10 +0000

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('_layout.base')
+@section('body')
+    <header style="background-image: url('/images/background-tutorials.jpg')">
+        <h1>404</h1>
+        <p>Page not found</p>
+    </header>
+    <section>
+        <div class="article">
+            <h2>Well this is a little embarassing</h2>
+            <p>I couldn't find the page you were looking for. Sorry about that. If you want to help, <a href="mailto:hello&amp;jamie.sh">shoot me an email</a>, otherwise try the navigation to the left of the page!</p>
+        </div>
+    </section>
+@stop
