a4e3e4d14a3617834bfb0c9671fcde3fd95fe62d
Jamie Shepherd <email@jamie.sh>
2015-04-01 00:18:51 +0100

    Changes

--- resources/views/_layout/base.blade.php
+++ resources/views/_layout/base.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
         <title>Jamie Shepherd, Designer &amp; Developer @if(isset($title)) - {{ $title }}@endif</title>
+        <meta name="description" content="Jamie Shepherd is a designer, developer, technology enthusiast, and computer science student"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/default.min.css">
         <link media="all" type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link media="all" type="text/css" rel="stylesheet" href="{{ elixir("css/app.css") }}">
@@ -16,4 +19,3 @@
 <div id="wrapper">
 @yield('body')
 @include('_layout.footer')
-</div>
