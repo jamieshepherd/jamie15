f627dcae078e0e7469c424f91fdb408bbdde73de
Jamie Shepherd <email@jamie.sh>
2015-04-01 21:57:21 +0100

    Reset password

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,50 +1,14 @@
-@extends('app')
+@extends('_layout.base')
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
+@section('body')
+    <section>
+        <h2>Reset password</h2>
+        <form class="auth" action="/password/email" method="POST">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input type="text" name="email" placeholder="Email address">
 
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+            <button type="submit" value="Submit"><i class="fa fa-check-square-o"></i> Submit</button>
+        </form>
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Send Password Reset Link
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+    </section>
 @endsection
